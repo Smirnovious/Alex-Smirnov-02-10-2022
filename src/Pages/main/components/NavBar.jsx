2680f999e8bf67cf0a27385a3ddc9215e897632d
Smirnovious <alex0401@gmail.com>
2022-11-08 10:57:37 +0200

    name change + small bug fixes

--- src/Pages/main/components/NavBar.jsx
+++ src/Pages/main/components/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
 
   return (
     <div className='text-2xl flex justify-between dark:bg-gray-800'>
-      <p className='my-auto ml-2 dark:text-amber-200'>Abra Weather Task</p>
+      <p className='my-auto ml-2 dark:text-amber-200'>Weatherflix</p>
       <div className='flex flex-row items-center'>
           <Link to={'/'}>
             <button className='w-10 h-10 mr-2 dark:text-amber-200'><AiFillHome/></button>
